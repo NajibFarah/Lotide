fe8fa47003dfeee0f4a0b00f7c1ae919fca4a2dc
Najib <muhammed.najib.farah@gmail.com>
2021-11-30 00:51:38 +0000

    Everything

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ const head = function(array) {
 const assertEqual = require('./assertEqual');
 
 //TEST 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 module.exports = head;
